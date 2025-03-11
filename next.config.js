621c17200f38a4829739ecad564197224f9f717c
mecattaf <thomasmecattaf@gmail.com>
2025-03-11 14:05:03 +0100

    fix for cf pages 4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 import nextra from 'nextra'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
@@ -6,10 +8,10 @@ const withNextra = nextra({
   // Nextra configuration
   mdxOptions: {
     remarkPlugins: [
-      require('remark-math')
+      remarkMath
     ],
     rehypePlugins: [
-      require('rehype-katex')
+      rehypeKatex
     ]
   },
   // Disable flexsearch as we're using pagefind
